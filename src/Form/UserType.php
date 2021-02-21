c1d210ba6e93d4026901b57b620ef9191b43067c
Vitaliy Timashkov <VDTimashkov@yandex.ru>
2021-02-21 21:26:30 +0300

    add admin

---
+++ src/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email',EmailType::class, array(
+                'label' => 'Введите email'
+            ))
+            ->add('plainPassword',RepeatedType::class,array(
+                'type' => PasswordType::class,
+                    'first_options' =>array(
+                        'label' => 'Пароль'
+                    ),
+                    'second_options' => array(
+                        'label' => 'Повтор пароля'
+                    )
+
+            )
+            )->add('save',SubmitType::class,array('label'=>'сохранить'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
